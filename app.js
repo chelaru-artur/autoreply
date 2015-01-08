525a0875884b0e043e2ee6f0484f7662bfebe3d7
artur <chelaru.artur@gmail.com>
2015-01-08 21:27:46 +0200

    more automatic then was

--- app.js
+++ app.js
@@ -1,69 +1,100 @@
-var url = require('url')
+var url = require('url');
+var fs = require('fs');
 var Twitter = require('twitter');
-var CronJob = require('cron').CronJob;
+// get and set current directory
+//when starting in crone the current working directori is /user/home/
+var CWD = process.argv[1].split('/').slice(0, -1).join('/');
+process.chdir(CWD);
+
 
 var client = new Twitter({
-    consumer_key: 'hKgxd0lLBZ4ytTKKW1mCHmnLL',
-    consumer_secret: 'ELVOqmJQ1AEMXs2WHZqB2oiE9nEmZzl2owQitnWvhnqC1OTtaJ',
-    access_token_key: '2939314121-gJlJTracDFDhPeqcOKWQltuv89vY0TOAF2dUHqE',
-    access_token_secret: '1nKPTx4kmq0XEwm1alO1z5LXxnxjLe0XVBPmmAsuqxeeH'
+	consumer_key: 'e34EKG7g1ZEfq1yCaY7cY6N2H',
+	consumer_secret: '6uNqFk6il8au7feW8rE87WdrzYuAqPqgZMbWQG6n4dUrSOcNlq',
+	access_token_key: '2966488821-pO3NYNDldgWheNQrDnpTwyg2vP4yEChqFydbf0g',
+	access_token_secret: 'xu9tyicy54LbZWP2etpw6E874ZjqWLtHl6KJwDVvxJCUS'
 });
+var CONFIG = 'config.json';
+var config = getConfig();
+var QUERY  = 'very sad'; // search querry
+
+function reply(status, callback) {
+	var message = '@' + status.user.screen_name + ' ' + randomMsg();
+	client.post('/statuses/update.json', {
+			status: message,
+			in_reply_to_status_id: status.id_str
+		},
+		function(err, data) {
+			if (err !== null) {
+				callback(err);
+				return;
+			}
+			addLog('sent message: ' + data.text);
+			//			console.log(data);
+
+		});
+}
 
 
-var query = 'today was  bad day'
-var status = 'Dont worry, be happy!';
-var index = 0
-var k = 0;
+function getConfig() {
+
+	fs.writeFileSync(CONFIG, '', {
+		flag: 'a'
+	}); // create config file if doesn't exist else  append nothing
+	var config = fs.readFileSync(CONFIG, 'utf8'); // read config file;
+	// check if valid json
+	try {
+		var cfg = JSON.parse(config);
+	} catch (e) {
+		return {};
+	}
+	return cfg;
+}
+
+function addLog(str) {
+	var LOG = 'log.txt';
+	var date = new Date();
+	var msg = date + '  ' + str + '\n';
+	fs.writeFileSync(LOG, msg, {
+		flag: 'a'
+	});
+}
 
 
-var reply = function(statuses) {
+function randomMsg() {
+	var messages = JSON.parse(fs.readFileSync('messages.json', 'utf8'));
+	return messages[Math.floor(Math.random() * messages.length)];
+}
 
-    for (var i = 0; i < statuses.length; i++) {
-        var message = '@' + statuses[i].user.screen_name + ' ' + index + ') ' + status;
-        index++;
-        client.post('/statuses/update.json', {
-                status: message,
-                in_reply_to_status_id: statuses[i].id_str
-            },
-            function(err, data) {
-                k++;
-                console.log(err);
-                console.log('sent to: %d statuses ', k);
-            });
-    };
+var search = function() {
+	var params = (config.params) ? config.params : {
+		q: QUERY,
+		count: 8
+	};
+
+	client.get('/search/tweets.json', params,
+		function(err, data) {
+			if (err) addLog(JSON.stringify(err));
+			// reply statuses , if err then it will be saved to log.txt
+			data.statuses.forEach(function(status) {
+				reply(status, function(err) {
+					if (err) {
+						addLog(JSON.stringify(err.data));
+						return;
+					}
+				});
+			});
+			// set params for next search and write them to config.json
+			if (data.search_metadata.next_results) {
+				config.params = url.parse(data.search_metadata.next_results, true).query;
+			} else {
+				addLog('no more data');
+			}
+			fs.writeFile(CONFIG, JSON.stringify(config), function(err) {
+				if (err) throw err;
+			});
+
+		});
 };
 
-var search = function(params) {
-    client.get('/search/tweets.json', params,
-        function(err, data) {
-            if (data.search_metadata.next_results != undefined) {
-                parms = url.parse(data.search_metadata.next_results, true).query; // get params 
-                reply(data.statuses);
-            } else {
-                if (data.statuses.length > 0)
-                    reply(data.statuses);
-            }
-        });
-};
 
-var params = {
-    q: query,
-    count: 20
-}; ///initialize params q: search query, count: numer of returnet statuses
-
-var CronJob = require('cron').CronJob;
-
-var job = new CronJob({
-    cronTime: '* 0,30 * * * *',
-    onTick: function() {
-        var date = new Date();
-        var hour = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-        console.log(hour, ' : ', min, ' : ', sec);
-        // runs every  minute
-        search(params);
-    },
-    start: true
-});
-job.start();
+search();
---
+++ readme.md
@@ -0,0 +1,1 @@
+just add app.js to crontab. Script was tested on every 10 minutes.
